63fdae2335153de46ce88a021621977506b98262
iwasnoob-dotcom <ahmadafandy660@gmail.com>
2023-08-17 12:50:54 +0800

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function keyIn() {
+  alert('Zawoorldo');
+}
+
+
+function clickme() {
+  const img = document.createElement("img");
+  img.src = "/res/img/wallhaven-orange_sun_1920x1080.png";
+  img.alt = "Orange Sun";
+  img.clientWidth = "300"
+  img.clientHeight = "300"
+  return document.body ? document.body.appendChild(img) : document.body.appendChild(img);
+
+}
+
+function submit() {
+  const s = document.getElementsByTagName("button");
+  return alert(s);
+
+}
